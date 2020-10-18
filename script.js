1d22eae527cbda58d5f83529ff191b6089f1c00c
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-18 18:39:30 -0400

    created function for the css element: past, present, and future. The the buttons will now can log the user's data but cannot store the data yet

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    //shows the current date
     $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
 
+    // function for tracking the time for the current day for the past, present, and future
+    function hourTracker() {
+        //get number of hrs
+        var currentHour = moment().hour()
+
+    }
+
+    // assign saveBtn to store the user's data
+    $(".saveBtn").on("click", function () {
+        console.log(this)
+    })
 })
